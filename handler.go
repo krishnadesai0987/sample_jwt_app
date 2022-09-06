0109f225b83f994eac2b7723424ea8756b903a0d
krishnadesai0987 <109665492+krishnadesai0987@users.noreply.github.com>
2022-09-06 15:47:27 +0530

    Update handler.go

--- handler.go
+++ handler.go
@@ -1,1 +1,40 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+// secret key
+var jwtKey = []byte("secret_key")
+
+var users = map[string]string{
+	"user1": "pass1",
+	"user2": "pass2",
+}
+
+//we can thepass the data as username and password from the apis and use it over here
+type Credentials struct {
+	Username string `json: "username"`
+	Password string `json:"password" `
+}
+
+// claims struct; claims will be use to create a payload for JWT.
+//inside the payload we will pass our username and exp
+
+type Claims struct {
+	Username string `json:"username"`
+	//jwt.StandardClaims
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+
+}
+func Home(w http.ResponseWriter, r *http.Request) {
+
+}
+func Refresh(w http.ResponseWriter, r *http.Request) {
+
+}
 
